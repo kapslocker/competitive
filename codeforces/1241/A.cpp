4bbfcc3bbe302f264dd732d480ae9b4d567f0e1c
Kapil Ahuja <kapil99a@gmail.com>
2019-10-08 20:02:42 +0530

    Technocup before DDos

---
+++ codeforces/1241/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+
+
+template<class L, class R> ostream &operator<<(ostream &os, pair<L,R> P) {
+    return os << "(" << P.first << "," << P.second << ")";
+}
+template<class T> ostream &operator<<(ostream &os, set<T> V) {
+    os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+template<class T> ostream &operator<<(ostream& os, vector<T> V) {
+    os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+template<class K, class X> ostream &operator<<(ostream& os, map<K,X> V) {
+    os << "["; for (auto vv : V) os << vv << ","; return os << "]";
+}
+
+void debug_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail...T) { cerr << " " << H; debug_out(T...); }
+
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+
+int32_t main() {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int q;
+    cin >> q;
+    while(q--) {
+        int n;
+        cin >> n;
+        if(n == 2) {
+            cout << 2 << endl;
+        }
+        else if(n % 2 == 0)
+            cout << 0 << endl;
+        else
+            cout << 1 << endl;
+    }
+    return 0;
+}
